75d6eec4513631e83bec9484a70a99cbac9a2805
Dave Salmon <davesalmon@users.noreply.github.com>
2016-01-13 08:21:07 -0800

    added

---
+++ Anagrammer/Anagrammer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Anagrammer
+//
+//  Created by David Salmon on 1/1/16.
+//  Copyright (c) 2016 Dave Salmon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
